c3afa9210d013fd9905b1df5df18eb4f8efe4f89
coyote <megan.metelak@gmail.com>
2022-02-28 11:47:26 -0600

    server running, seeds set up

--- server.js
+++ server.js
@@ -22,20 +22,20 @@ const sess = {
 };
 
 app.use(session(sess));
-// import helpers functions, tell Handlebars.js about the helpers file
-const helpers = require('./utils/helpers');
-// pass helpers to create
-const hbs = exphbs.create({ helpers });
+// // import helpers functions, tell Handlebars.js about the helpers file
+// const helpers = require('./utils/helpers');
+// // pass helpers to create
+// const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// turn on path to public folder - stylesheets
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// // turn on path to public folder - stylesheets
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
