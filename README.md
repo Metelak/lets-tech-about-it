222421896acaf3f653b1e37a6727fcb614cb6727
coyote <megan.metelak@gmail.com>
2022-02-28 09:58:05 -0600

    server and connection files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lets-tech-about-it
+**Challenge 14**
 A CMS blog site for developers who write about tech, allow them to publish articles, blog posts, and express thoughts and opinions
---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+// connect/use express-session and sequelize store
+const session = require('express-session');
+// connecting handlebars as template engine of choice
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'dancing umbrellas',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+// import helpers functions, tell Handlebars.js about the helpers file
+const helpers = require('./utils/helpers');
+// pass helpers to create
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// turn on path to public folder - stylesheets
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
